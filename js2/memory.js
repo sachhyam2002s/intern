a80b8089fd9397894d54f7428788fc4427bb3dea
sakshyam <sachhyam28@gmail.com>
2025-03-05 17:58:43 +0545

    js memory

---
+++ js2/memory.js
@@ -0,0 +1,18 @@
+// ******stack(primitive) memory****** //
+let firstName = "first"
+let anotherName = firstName //copys firstName's name
+anotherName = "another" //new name is given to anotherName but firstName remains same
+console.log(firstName);
+console.log(anotherName);
+
+
+// ******heap(non-primitive) memory****** //
+let userOne = {     //contents inside {} is stored in heap memory
+    email: "userOne@gmail.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne   //gets userOne's data
+userTwo.email = "userTwo@gmail.com" //changes both userOne and userTwo email
+console.log(userOne.email);
+console.log(userTwo.email);
+
